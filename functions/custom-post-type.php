08283e89265fe3287a9afe17a012cde930d56802
Ben Everard <hello@beneverard.co.uk>
2016-01-14 14:53:18 +0000

    Created all initial custom post types

--- functions/custom-post-type.php
+++ functions/custom-post-type.php
@@ -28,11 +28,77 @@ function create_post_type_labels($singular, $plural = NULL) {
  //*****************
 // CUSTOM POST TYPE
 
+register_post_type('news', array(
+	'labels'				=> create_post_type_labels('News', 'News'),
+	'public'				=> TRUE,
+	'query_var'				=> TRUE,
+	'rewrite'				=> array('slug' => 'news', 'with_front' => FALSE),
+	'capability_type'		=> 'post',
+	'taxonomies'			=> array(),
+	'supports'				=> array('title', 'editor', 'thumbnail'),
+	'has_archive'			=> FALSE
+));
+
+register_post_type('event', array(
+	'labels'				=> create_post_type_labels('Event'),
+	'public'				=> TRUE,
+	'query_var'				=> TRUE,
+	'rewrite'				=> array('slug' => 'event', 'with_front' => FALSE),
+	'capability_type'		=> 'post',
+	'taxonomies'			=> array(),
+	'supports'				=> array('title', 'editor', 'thumbnail'),
+	'has_archive'			=> FALSE
+));
+
 register_post_type('resource', array(
 	'labels'				=> create_post_type_labels('Resource'),
 	'public'				=> TRUE,
 	'query_var'				=> TRUE,
 	'rewrite'				=> array('slug' => 'resource', 'with_front' => FALSE),
+	'capability_type'		=> 'post',
+	'taxonomies'			=> array(),
+	'supports'				=> array('title', 'editor', 'thumbnail'),
+	'has_archive'			=> FALSE
+));
+
+register_post_type('success-story', array(
+	'labels'				=> create_post_type_labels('Success Story', 'Success Stories'),
+	'public'				=> TRUE,
+	'query_var'				=> TRUE,
+	'rewrite'				=> array('slug' => 'success-story', 'with_front' => FALSE),
+	'capability_type'		=> 'post',
+	'taxonomies'			=> array(),
+	'supports'				=> array('title', 'editor', 'thumbnail'),
+	'has_archive'			=> FALSE
+));
+
+register_post_type('policy', array(
+	'labels'				=> create_post_type_labels('Policy', 'Policies'),
+	'public'				=> TRUE,
+	'query_var'				=> TRUE,
+	'rewrite'				=> array('slug' => 'policy', 'with_front' => FALSE),
+	'capability_type'		=> 'post',
+	'taxonomies'			=> array(),
+	'supports'				=> array('title', 'editor', 'thumbnail'),
+	'has_archive'			=> FALSE
+));
+
+register_post_type('newsletter', array(
+	'labels'				=> create_post_type_labels('Newsletter'),
+	'public'				=> TRUE,
+	'query_var'				=> TRUE,
+	'rewrite'				=> array('slug' => 'newsletter', 'with_front' => FALSE),
+	'capability_type'		=> 'post',
+	'taxonomies'			=> array(),
+	'supports'				=> array('title', 'editor', 'thumbnail'),
+	'has_archive'			=> FALSE
+));
+
+register_post_type('media-clipping', array(
+	'labels'				=> create_post_type_labels('Media Clipping'),
+	'public'				=> TRUE,
+	'query_var'				=> TRUE,
+	'rewrite'				=> array('slug' => 'media-clipping', 'with_front' => FALSE),
 	'capability_type'		=> 'post',
 	'taxonomies'			=> array(),
 	'supports'				=> array('title', 'editor', 'thumbnail'),
