15487f151c372622599d53959894c05fc6da04aa
Ben Everard <hello@beneverard.co.uk>
2016-01-20 12:27:54 +0000

    Added vue.php to fetch posts and populate important data

---
+++ functions/vue.php
@@ -0,0 +1,60 @@
+<?php
+
+function vue_posts($args) {
+
+	$query = new query_loop($args['query']);
+	$vue_posts = [];
+	$post_ids = [];
+
+
+	foreach ( $query as $vue_post ) {
+
+		// track the post ID
+		$post_ids[] = $vue_post->ID;
+
+		$vue_posts[$vue_post->ID] = array(
+			'id' => get_the_ID(),
+			'title' => get_the_title(),
+			'slug' => basename(get_the_permalink()),
+			'content' => get_the_content(),
+			'excerpt' => get_the_excerpt(),
+			'link' => get_the_permalink(),
+			'taxonomies' => array(),
+			'custom' => array(),
+			'display' => true
+		);
+
+		if ( isset($args['taxonomies']) && ! empty($args['taxonomies']) ) {
+
+			// loop through taxonomies
+			foreach ( $args['taxonomies'] as $taxonomy ) {
+
+				$vue_posts[$vue_post->ID]['taxonomies'][$taxonomy] = array();
+
+				if ( $terms = get_the_terms($vue_post->ID, $taxonomy) ) {
+
+					foreach ( $terms as $term ) {
+						$vue_posts[$vue_post->ID]['taxonomies'][$taxonomy][$term->slug] = $term->name;
+					}
+
+				}
+
+			}
+
+		}
+
+		if ( isset($args['custom']) ) {
+
+			foreach ( $args['custom'] as $custom => $callback ) {
+				$vue_posts[$vue_post->ID][$custom] = $callback($vue_post);
+			}
+
+		}
+
+	}
+
+	$vue_posts = array_values($vue_posts);
+
+	return $vue_posts;
+
+}
