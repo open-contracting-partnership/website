d3e15a03ac0883d69bcf9918bf2e0888fffd260e
Ben Everard <hello@beneverard.co.uk>
2016-01-21 15:19:41 +0000

    Added rudimentary structure and styling to resources archive

--- archive-resource.php
+++ archive-resource.php
@@ -2,62 +2,66 @@
 
 <?php get_header(); ?>
 
+	<?php
+
+		$resources = vue_posts([
+			'query' => array(
+				'post_type' => 'resource',
+				'posts_per_page' => -1
+			),
+			'taxonomies' => ['resource-type', 'country', 'region'],
+			'fields' => ['short_description'],
+			'custom' => array(
+				'year' => function($vue_post) {
+					return date('Y', strtotime($vue_post->post_date));
+				}
+
+			)
+		]);
+
+		$resource_types = array_values(get_terms('resource-type'));
+
+	?>
+
 	<div id="resources">
 
 		<input type="search" placeholder="Search" v-model="search">
 
-		<h1>Resources</h1>
+		<p class="resources__popular">This week's popular items are <a href="#">report 1</a>, <a href="#">report 2</a> and <a href="#">report 3</a></p>
 
-		<?php
+		<div class="resource__filter-container">
 
-			$resources = vue_posts([
-				'query' => array(
-					'post_type' => 'resource',
-					'posts_per_page' => -1
-				),
-				'taxonomies' => ['resource-type', 'country', 'region'],
-				'fields' => [],
-				'custom' => array(
-					'year' => function($vue_post) {
-						return date('Y', strtotime($vue_post->post_date));
-					}
+			<div class="resource__filter">
 
-				)
-			]);
+				<label>
+					2014
+					<input type="checkbox" value="2014" v-model="filter_years" />
+				</label>
+				<label>
+					2015
+					<input type="checkbox" value="2015" v-model="filter_years" />
+				</label>
+				<label>
+					2016
+					<input type="checkbox" value="2016" v-model="filter_years" />
+				</label>
 
-			$resource_types = array_values(get_terms('resource-type'));
+			</div>
 
-		?>
+			<div class="resource__filter">
 
-		<p>
+				<label v-for="resource_type in resource_types">
+					{{ resource_type.name }}
+					<input type="checkbox" value="{{ resource_type.slug }}" v-model="filter_resource_type" />
+				</label>
 
-			<label>
-				2014
-				<input type="checkbox" value="2014" v-model="filter_years" />
-			</label>
-			<label>
-				2015
-				<input type="checkbox" value="2015" v-model="filter_years" />
-			</label>
-			<label>
-				2016
-				<input type="checkbox" value="2016" v-model="filter_years" />
-			</label>
+			</div>
 
-		</p>
-
-		<p>
-
-			<label v-for="resource_type in resource_types">
-				{{ resource_type.name }}
-				<input type="checkbox" value="{{ resource_type.slug }}" v-model="filter_resource_type" />
-			</label>
-
-		</p>
+		</div> <!-- / .resource__filter-container -->
 
 		<div v-if="visibleResources.length">
 
-			<div v-for="resource in visibleResources">
+			<div v-for="resource in visibleResources" class="resource">
 				<resource :resource="resource"></resource>
 			</div>
 
@@ -67,10 +71,17 @@
 
 		<template id="resource-template">
 
-			<div class="box">
-				<h1><a href="{{resource.link}}">{{resource.title}}</a></h1>
-				<pre>{{ resource | json }}</pre>
-			</div>
+			<a href="{{resource.link}}">
+				<img src="http://placehold.it/116x151" />
+			</a>
+
+			<h1><a href="{{resource.link}}">{{resource.title}}</a></h1>
+
+			<p>
+				<a v-for="type in resource.taxonomies['resource-type']" href="/resource-type/{{ $key }}">{{ type }}</a>
+			</p>
+
+			{{{ resource.fields.short_description }}}
 
 		</template>
 
@@ -83,9 +94,36 @@
 				template: '#resource-template',
 				props: {
 					resource: Object
-				}
-			})
+				},
+				methods: {
 
+					taxonomyLabels: function(taxonomy) {
+
+
+
+
+						return objectValues(this.resource.taxonomies[taxonomy]);
+					}
+
+				}
+			});
+
+function objectValues(object) {
+
+	var array = [];
+
+	Object.keys(object).forEach(function (key) {
+		array.push(object[key]);
+	});
+
+	return array;
+
+}
+
+Vue.filter('objectValues', function (object) {
+	console.log(objectValues(object));
+  return objectValues(object);
+});
 			// bootstrap the demo
 			var demo = new Vue({
 
@@ -193,9 +231,18 @@
 
 					}
 
+				},
+
+				filters: {
+
+					objectValues: function() {
+						console.log('bop');
+					}
+
 				}
 
 			});
+
 
 			var intersection = function (haystack, arr) {
 				return arr.some(function (v) {
