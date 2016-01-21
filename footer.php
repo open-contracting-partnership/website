3a0ee0c44324b980cbb7d2927fda7747d110b4a8
Connor Holyday <connorholyday@gmail.com>
2016-01-21 15:42:56 +0000

    Initial footer markup

--- footer.php
+++ footer.php
@@ -1,6 +1,251 @@
 <?php // footer.php ?>
 
+		<div class="site-footer">
+
+			<div>
+
+				<div>
+
+					<nav>
+
+						<h4>Our Organisation</h4>
+
+						<ul class="nav nav--vertical">
+
+							<li>
+								<a href="#">Mission</a>
+							</li>
+
+							<li>
+								<a href="#">Strategy</a>
+							</li>
+
+							<li>
+								<a href="#">Values</a>
+							</li>
+
+							<li>
+								<a href="#">Our Story</a>
+							</li>
+
+							<li>
+								<a href="#">Team</a>
+							</li>
+
+							<li>
+								<a href="#">Advisory board</a>
+							</li>
+
+							<li>
+								<a href="#">Financing</a>
+							</li>
+
+							<li>
+								<a href="#">Policies</a>
+							</li>
+
+						</ul>
+
+					</nav>
+
+				</div>
+
+				<div>
+
+					<nav>
+
+						<h4>Open Contracting</h4>
+
+						<ul class="nav nav--vertical">
+
+							<li>
+								<a href="#">What is Open Contracting</a>
+							</li>
+
+							<li>
+								<a href="#">Sectors</a>
+							</li>
+
+							<li>
+								<a href="#">Showcase projects</a>
+							</li>
+
+							<li>
+								<a href="#">Evidence</a>
+							</li>
+
+							<li>
+								<a href="#">Learning</a>
+							</li>
+
+							<li>
+								<a href="#">Case studies</a>
+							</li>
+
+						</ul>
+
+					</nav>
+
+				</div>
+
+				<div>
+
+					<nav>
+
+						<h4>Implement</h4>
+
+						<ul class="nav nav--vertical">
+
+							<li>
+								<a href="#">How to implement</a>
+							</li>
+
+							<li>
+								<a href="#">Data standard</a>
+							</li>
+
+							<li>
+								<a href="#">Success stories</a>
+							</li>
+
+							<li>
+								<a href="#">Global principles</a>
+							</li>
+
+							<li>
+								<a href="#">Learning</a>
+							</li>
+
+							<li>
+								<a href="#">Case studies</a>
+							</li>
+
+							<li>
+								<a href="#">Resources and Tools</a>
+							</li>
+
+						</ul>
+
+					</nav>
+
+				</div>
+
+				<div>
+
+					<nav>
+
+						<h4>Stay Updated</h4>
+
+						<ul class="nav nav--vertical">
+
+							<li>
+								<a href="#">Blogs and Updates</a>
+							</li>
+
+							<li>
+								<a href="#">Events</a>
+							</li>
+
+						</ul>
+
+					</nav>
+
+					<nav>
+
+						<h4>Elsewhere Online</h4>
+
+						<ul class="nav nav--vertical">
+
+							<li>
+								<a href="#">Data Standard</a>
+							</li>
+
+							<li>
+								<a href="#">Community</a>
+							</li>
+
+						</ul>
+
+					</nav>
+
+				</div>
+
+				<div>
+
+					<div>
+
+						<h4>Subscribe to our newsletter</h4>
+
+						<form action="#">
+
+							<input type="email" name="newsletter" placeholder="Enter your email">
+
+							<button type="submit">Send</button>
+
+						</form>
+
+					</div>
+
+					<nav>
+
+						<h4>Follow our progress</h4>
+
+						<ul class="nav nav--horizontal">
+
+							<li>
+								<a href="#">Twitter</a>
+							</li>
+
+							<li>
+								<a href="#">Facebook</a>
+							</li>
+
+							<li>
+								<a href="#">Linkedin</a>
+							</li>
+
+						</ul>
+
+					</nav>
+
+					<div>
+
+						<h4>Contact us:</h4>
+
+						<address>
+							<span>641 S Street NW, 20001 Washington, D.C., USA<span>
+							<a href="mailto:engage@open-contracting.org">engage@open-contracting.org</a>
+						</address>
+
+					</div>
+
+				</div>
+
+			</div>
+
+			<div>
+
+				<div>
+					<p>This work by the Open Contracting Partnership, unless otherwise noted, is licensed under a Creative Commons Attribution 4.0 International License.</p>
+				</div>
+
+				<img src="http://placehold.it/300x100" alt="Logo" />
+
+			</div>
+
+		</div>
+
 		<footer>
+
+			<span>&copy; Open Contracting Partnership 2016</span>
+
+			<a href="#">Site Information</a>
+
+			<a href="#">Feedback</a>
+
+			<a href="#">Terms</a>
+
+			<span>Website by <a href="http://theideabureau.co">The Idea Bureau</a></span>
 
 		</footer>
 
