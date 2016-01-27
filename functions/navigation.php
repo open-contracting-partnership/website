292399a19f3323cd86e9aeec06e55e7c0f437656
Ben Everard <hello@beneverard.co.uk>
2016-01-27 15:22:41 +0000

    Added WP navigation locations

--- functions/navigation.php
+++ functions/navigation.php
@@ -16,3 +16,22 @@ function get_menu($name) {
 	return $menu;
 
 }
+
+
+ //*************
+// REGISTER NAV
+
+add_action('after_setup_theme', function() {
+
+	// header
+	register_nav_menu('header-primary', __('Header Primary', 'ocp'));
+	register_nav_menu('header-secondary', __('Header Secondary', 'ocp'));
+
+	// footer
+	register_nav_menu('footer-our-organisation', __('Footer: Our Organisation', 'ocp'));
+	register_nav_menu('footer-open-contracting', __('Footer: Open Contracting', 'ocp'));
+	register_nav_menu('footer-implement', __('Footer: Implement', 'ocp'));
+	register_nav_menu('footer-stay-updated', __('Footer: Stay Updated', 'ocp'));
+	register_nav_menu('footer-elsewhere-online', __('Footer: Elsewhere Online', 'ocp'));
+
+});
