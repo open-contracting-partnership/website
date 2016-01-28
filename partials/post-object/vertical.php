0ace680c0524aa4a87db50c141d62d2ff4b03c1e
Ben Everard <hello@beneverard.co.uk>
2016-01-28 11:06:23 +0000

    Improved vertical post object partial output

--- partials/post-object/vertical.php
+++ partials/post-object/vertical.php
@@ -1,4 +1,4 @@
-<a href="<?php the_permalink(); ?>" class="post-object post-object--vertical">
+<a class="post-object post-object--vertical" href="<?php the_permalink(); ?>">
 
 	<div class="post-object__media">
 
@@ -8,13 +8,12 @@
 			<img src="http://placehold.it/460x230" alt="">
 		<?php endif; ?>
 
-		<span class="post-object__tag">Projects</span>
+		<span class="post-object__tag"><?php the_post_type_label(); ?></span>
 
 	</div>
 
 	<div class="post-object__content">
 		<h4><?php the_title(); ?></h4>
-		<p>Open is part of our name and game at the Open Contracting Partnership. This means, open is at the core of our institution. It is our mission. It is one of our core values. If we want public procurement to be open, we have to be open ourselves.</p>
 	</div>
 
 </a>
