ffbd5f392d3cbb90d790532ad9d22f9c647d3ff6
Ben Everard <hello@beneverard.co.uk>
2016-02-02 10:38:26 +0000

    Added wrapper to front-page

--- front-page.php
+++ front-page.php
@@ -2,166 +2,162 @@
 
 <?php get_header(); ?>
 
-	<?php/* if ( have_posts() ) : the_post(); ?>
+	<div class="wrapper">
 
-		<h1><?php the_title(); ?></h1>
+		<div class="homepage-cta / band band--section">
 
-		<?php the_content(); ?>
+			<div class="highlight-text / band">
+				<p>The Open Contracting Partnership connects governments, civil society and business to open up and monitor public contracting. The result: Better deals, less fraud and corruption, higher-quality services, and a fairer business environment.</p>
+			</div>
 
-	<?php endif; */?>
+			<a href="" class="button button--white">About Our Work</a>
+			<a href="" class="button button--white">Visit the Data Standard</a>
 
-	<div class="homepage-cta / band band--section">
+		</div> <!-- homepage-cta -->
 
-		<div class="highlight-text / band">
-			<p>The Open Contracting Partnership connects governments, civil society and business to open up and monitor public contracting. The result: Better deals, less fraud and corruption, higher-quality services, and a fairer business environment.</p>
-		</div>
+		<section class="g-break / band band--section">
 
-		<a href="" class="button button--white">About Our Work</a>
-		<a href="" class="button button--white">Visit the Data Standard</a>
+			<div class="homepage-title">
+				<h1>Public contracting</h1>
+				<h2>The number one corruption risk in government</h2>
+			</div>
 
-	</div> <!-- homepage-cta -->
+			<div class="stat-module / l-4 g-first">
+				<span>$9.5 Trillion</span>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed sapien quam. Sed dapibus est id enim facilisis, at posuere turpis adipiscing. Quisque sit amet dui dui.</p>
+			</div>
 
-	<section class="g-break / band band--section">
+			<div class="stat-module / l-4">
+				<span>40</span>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed sapien quam. Sed dapibus est id enim facilisis, at posuere turpis adipiscing. Quisque sit amet dui dui.</p>
+			</div>
 
-		<div class="homepage-title">
-			<h1>Public contracting</h1>
-			<h2>The number one corruption risk in government</h2>
-		</div>
+			<div class="stat-module / l-4 g-last">
+				<span>4</span>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed sapien quam. Sed dapibus est id enim facilisis, at posuere turpis adipiscing. Quisque sit amet dui dui.</p>
+			</div>
 
-		<div class="stat-module / l-4 g-first">
-			<span>$9.5 Trillion</span>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed sapien quam. Sed dapibus est id enim facilisis, at posuere turpis adipiscing. Quisque sit amet dui dui.</p>
-		</div>
+		</section> <!-- stat-modules -->
 
-		<div class="stat-module / l-4">
-			<span>40</span>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed sapien quam. Sed dapibus est id enim facilisis, at posuere turpis adipiscing. Quisque sit amet dui dui.</p>
-		</div>
+		<div class="band band--thicker">
 
-		<div class="stat-module / l-4 g-last">
-			<span>4</span>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed sapien quam. Sed dapibus est id enim facilisis, at posuere turpis adipiscing. Quisque sit amet dui dui.</p>
-		</div>
+			<div class="homepage-blogs / l-4 g-first">
 
-	</section> <!-- stat-modules -->
+				<section>
 
-	<div class="band band--thicker">
+					<h3 class="border-top">Recent Blogs</h3>
 
-		<div class="homepage-blogs / l-4 g-first">
+					<?php
 
-			<section>
+						$recent_posts = new query_loop([
+							'post_type' => 'post',
+							'posts_per_page' => 3
+						]);
 
-				<h3 class="border-top">Recent Blogs</h3>
+					?>
 
-				<?php
+					<?php foreach ( $recent_posts as $recent_post ) : ?>
 
-					$recent_posts = new query_loop([
-						'post_type' => 'post',
-						'posts_per_page' => 3
-					]);
+						<a href="<?php the_permalink(); ?>" class="post-object post-object--horizontal / media">
 
-				?>
+							<div href="#" class="post-object__media / media__object">
+								<img src="http://placehold.it/122x154" alt="">
+							</div>
 
-				<?php foreach ( $recent_posts as $recent_post ) : ?>
+							<div class="post-object__content / media__body">
+								<h4><?php the_title(); ?></h4>
+								<p>By <?php the_authors(); ?></p>
+							</div>
 
-					<a href="<?php the_permalink(); ?>" class="post-object post-object--horizontal / media">
+						</a>
 
-						<div href="#" class="post-object__media / media__object">
-							<img src="http://placehold.it/122x154" alt="">
+					<?php endforeach; ?>
+
+				</section>
+
+			</div> <!-- homepage-blogs -->
+
+			<div class="homepage-spotlight / l-4">
+
+				<section>
+
+					<h3 class="border-top">Spotlight</h3>
+
+					<a href="#" class="post-object post-object--vertical">
+
+						<div class="post-object__media">
+							<img src="http://placehold.it/459x235" alt="">
+							<span class="post-object__tag">Projects</span>
 						</div>
 
-						<div class="post-object__content / media__body">
-							<h4><?php the_title(); ?></h4>
-							<p>By <?php the_authors(); ?></p>
+						<div class="post-object__content">
+							<h4>Help us define our openness</h4>
+							<p>Open is part of our name and game at the Open Contracting Partnership. This means, open is at the core of our institution. It is our mission. It is one of our core values. If we want public procurement to be open, we have to be open ourselves.</p>
 						</div>
 
 					</a>
 
-				<?php endforeach; ?>
+				</section>
 
-			</section>
+			</div> <!-- homepage-spotlight -->
 
-		</div> <!-- homepage-blogs -->
+			<div class="homepage-implement / l-3">
 
-		<div class="homepage-spotlight / l-4">
+				<section>
 
-			<section>
-
-				<h3 class="border-top">Spotlight</h3>
-
-				<a href="#" class="post-object post-object--vertical">
-
-					<div class="post-object__media">
-						<img src="http://placehold.it/459x235" alt="">
-						<span class="post-object__tag">Projects</span>
+					<div class="heading-highlight">
+						<h3>Implement Open Contracting</h3>
 					</div>
 
-					<div class="post-object__content">
-						<h4>Help us define our openness</h4>
-						<p>Open is part of our name and game at the Open Contracting Partnership. This means, open is at the core of our institution. It is our mission. It is one of our core values. If we want public procurement to be open, we have to be open ourselves.</p>
-					</div>
+					<span>Learn how to implement open contracting or use contracting data.</span>
 
-				</a>
+					<ul class="cta-list">
+						<li><a href="#">Showcases and evidence</a></li>
+						<li><a href="#">How to get started</a></li>
+						<li><a href="#">Global guidelines</a></li>
+					</ul>
 
-			</section>
+				</section>
 
-		</div> <!-- homepage-spotlight -->
+			</div> <!-- homepage-implement -->
 
-		<div class="homepage-implement / l-3">
+		</div>
 
-			<section>
+		<section class="g-break">
 
-				<div class="heading-highlight">
-					<h3>Implement Open Contracting</h3>
-				</div>
+			<a href="" class="homepage-filter / band band--thicker">Filter Blogs &amp; Updates</a>
+
+			<div class="l-4 g-first"></div>
+			<div class="l-4"></div>
+			<div class="l-4 g-last"></div>
+			<div class="l-4 g-first"></div>
+			<div class="l-4"></div>
+			<div class="l-4 g-last"></div>
+
+		</section>
+
+		<section class="homepage-map / band band--section">
+
+			<div class="l-7 g-first">
+				<img src="http://placehold.it/459x400" alt="">
+			</div>
+
+			<div class="l-5 g-last">
+
+				<h3>Worldwide</h3>
 
 				<span>Learn how to implement open contracting or use contracting data.</span>
 
 				<ul class="cta-list">
-					<li><a href="#">Showcases and evidence</a></li>
-					<li><a href="#">How to get started</a></li>
-					<li><a href="#">Global guidelines</a></li>
+					<li><a href="#">World Overview</a></li>
+					<li><a href="#">Showcase on Ukraine</a></li>
+					<li><a href="#">Showcase on Mexico</a></li>
 				</ul>
 
-			</section>
+			</div>
 
-		</div> <!-- homepage-implement -->
+		</section>
 
-	</div>
-
-	<section class="g-break">
-
-		<a href="" class="homepage-filter / band band--thicker">Filter Blogs &amp; Updates</a>
-
-		<div class="l-4 g-first"></div>
-		<div class="l-4"></div>
-		<div class="l-4 g-last"></div>
-		<div class="l-4 g-first"></div>
-		<div class="l-4"></div>
-		<div class="l-4 g-last"></div>
-
-	</section>
-
-	<section class="homepage-map / band band--section">
-
-		<div class="l-7 g-first">
-			<img src="http://placehold.it/459x400" alt="">
-		</div>
-
-		<div class="l-5 g-last">
-
-			<h3>Worldwide</h3>
-
-			<span>Learn how to implement open contracting or use contracting data.</span>
-
-			<ul class="cta-list">
-				<li><a href="#">World Overview</a></li>
-				<li><a href="#">Showcase on Ukraine</a></li>
-				<li><a href="#">Showcase on Mexico</a></li>
-			</ul>
-
-		</div>
-
-	</section>
+	</div> <!-- / .wrapper -->
 
 <?php get_footer(); ?>
