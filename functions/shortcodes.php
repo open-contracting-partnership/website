6aae3ed6819db5fc983fc467ecae1997cb56f317
Ben Everard <hello@beneverard.co.uk>
2016-02-03 13:42:03 +0000

    Output sections within page

--- functions/shortcodes.php
+++ functions/shortcodes.php
@@ -1,1 +1,29 @@
 <?php // functions/shortcodes.php
+
+add_shortcode('section', function($atts, $content = null) {
+	return '<p class="section__title" id="' . sanitize_title($content) . '"><span class="button">' . $content . '</span></p>';
+});
+
+function get_sections() {
+
+	global $post;
+	$pattern = get_shortcode_regex();
+	preg_match_all('/'.$pattern.'/s', $post->post_content, $matches, PREG_SET_ORDER);
+
+	$sections = array();
+
+	if ( is_array($matches) ) {
+
+		foreach ( $matches as $match ) {
+
+			if ( $match[2] === 'section' ) {
+				$sections[sanitize_title($match[5])] = $match[5];
+			}
+
+		}
+
+	}
+
+	return $sections;
+
+}
--- page.php
+++ page.php
@@ -4,6 +4,8 @@
 
 	<?php the_post(); ?>
 
+	<?php $sections = get_sections(); ?>
+
 	<div class="wrapper">
 
 		<?php get_partial('breadcrumbs'); ?>
@@ -22,17 +24,23 @@
 
 		</nav>
 
-		<nav>
+		<?php if ( $sections ) : ?>
 
-			<h3 class="border-top border-top--clean">Jump to section</h3>
+			<nav>
 
-			<ul class="nav nav--vertical nav--in-page">
-				<li class="active"><a href="#">What we do</a></li>
-				<li><a href="#">Why we do it</a></li>
-				<li><a href="#">Our Story</a></li>
-			</ul>
+				<h3 class="border-top border-top--clean">Jump to section</h3>
 
-		</nav>
+				<ul class="nav nav--vertical nav--in-page">
+
+					<?php foreach ( $sections as $id => $label ) : ?>
+						<li><a href="#<?php echo $id; ?>"><?php echo $label; ?></a></li>
+					<?php endforeach; ?>
+
+				</ul>
+
+			</nav>
+
+		<?php endif; ?>
 
 		<a href="#" class="download-cta">
 
@@ -45,123 +53,36 @@
 				<div class="heading-highlight">
 					<h4>2015 - 2018 Strategy document</h4>
 				</div>
-				
+
 			</div>
 
 		</a>
 
 		<article class="cf">
 
+			<?php
+
+				$title = get_the_title();
+
+				if ( $sections = get_sections() ) {
+					$title = current($sections);
+				}
+
+			?>
+
 			<div class="blog__title">
-				<h1><?php the_title(); ?></h1>
+
+				<?php if ( $sections ) : ?>
+					<h1 id="<?php echo key($sections); ?>"><?php echo current($sections); ?></h1>
+				<?php else : ?>
+					<h1><?php the_title(); ?></h1>
+				<?php endif; ?>
+
 			</div>
 
 			<section>
-
-				<p class="blog__mobile-date">Written By <?php the_authors(TRUE); ?>, <datetime><?php the_date(); ?></datetime></p>
-
-				<?php if ( has_post_thumbnail() ) : ?>
-					<?php the_post_thumbnail(); ?>
-				<?php endif; ?>
-
 				<?php the_content(); ?>
-
 			</section>
-
-			<aside class="sidebar">
-
-				<section>
-					<h3 class="border-top">Written By</h3>
-					<p><?php the_authors(TRUE); ?>, <time datetime="<?php the_time(DATE_W3C); ?>"><?php the_time(get_option('date_format')); ?></time></p>
-				</section>
-
-				<section>
-
-					<h3 class="border-top">Share</h3>
-
-					<ul class="button__list button__social">
-						<li><a href="#" class="button"><svg><use xlink:href="#icon-facebook" /></svg></a></li>
-						<li><a href="#" class="button"><svg><use xlink:href="#icon-linkedin" /></svg></a></li>
-						<li><a href="#" class="button"><svg><use xlink:href="#icon-twitter" /></svg></a></li>
-					</ul>
-
-				</section>
-
-				<?php if ( $open_contracting = get_field('open_contracting') ) : ?>
-
-					<?php
-
-						$query = array(
-							'post_type' => 'resource',
-							'posts_per_page' => 3,
-							'meta_query' => ['relation' => 'OR']
-						);
-
-						foreach ( $open_contracting as $value ) {
-
-							$query['meta_query'][] = array(
-								'key' => 'open_contracting',
-								'value' => '"' . $value . '"',
-								'compare' => "LIKE"
-							);
-
-						}
-
-						$relevant_resources = new query_loop($query);
-
-					?>
-
-					<?php if ( $relevant_resources->have_posts() ) : ?>
-
-						<section>
-
-							<h3 class="border-top">Relevant Resources</h3>
-
-							<ul class="relevant-resources">
-
-								<?php foreach ( $relevant_resources as $relevant_resource ) : ?>
-									<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
-								<?php endforeach; ?>
-
-							</ul>
-
-						</section>
-
-					<?php endif; ?>
-
-				<?php endif; ?>
-
-				<?php
-					$post_tags = wp_get_post_terms(get_the_ID(), 'post_tag', array(
-						'orderby' => 'count',
-						'order' => 'DESC'
-					));
-
-				?>
-
-				<?php if ( $post_tags ) : ?>
-
-					<section>
-
-						<h3 class="border-top">Related Tags</h3>
-
-						<ul class="button__list">
-
-							<?php foreach ( $post_tags as $post_tag ) : ?>
-
-								<li>
-									<a href="<?php echo get_term_link($post_tag); ?>" class="button button--uppercase button--thick"><?php echo $post_tag->name; ?></a>
-								</li>
-
-							<?php endforeach; ?>
-
-						</ul>
-
-					</section>
-
-				<?php endif; ?>
-
-			</aside>
 
 		</article>
 
