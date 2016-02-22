99f45dd2dc8261004d5fdab925de453c06d1860c
Connor Holyday <connorholyday@gmail.com>
2016-02-22 15:13:36 +0000

    Implemented frontend of page-strip section

---
+++ partials/page/strip.php
@@ -0,0 +1,19 @@
+<section class="page-strip page-strip--reverse page-strip--brand page-strip--teal page-strip--dark-background page-strip--background">
+
+	<div class="wrapper">
+
+		<div class="page-strip__media">
+			<img src="//placehold.it/500x350" alt="" />
+		</div>
+
+		<div class="page-strip__content">
+
+			<h3>The Title</h3>
+
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+		</div>
+
+	</div>
+
+</section>
