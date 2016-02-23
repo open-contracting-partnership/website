ebd9923102b502ad0747558b5986261fbaa28e88
Connor Holyday <connorholyday@gmail.com>
2016-02-23 14:53:23 +0000

    Re-ordered front page

--- front-page.php
+++ front-page.php
@@ -21,6 +21,36 @@
 
 	</div>
 
+	<section class="homepage-contracting / band band--thick / island island--thick island--dark">
+
+		<div class="wrapper">
+
+			<div class="homepage-title">
+				<h1><?php pll_e('Public contracting'); ?></h1>
+				<h2><?php pll_e('The number one corruption risk in government'); ?></h2>
+			</div>
+
+			<?php if ( have_rows('statistics') ) : ?>
+
+				<div class="stat__container">
+
+					<?php while ( have_rows('statistics') ) : the_row(); ?>
+
+						<div class="stat-module">
+							<span><?php the_sub_field('stat'); ?></span>
+							<p><?php the_sub_field('copy'); ?></p>
+						</div>
+
+					<?php endwhile; ?>
+
+				</div> <!-- / .stat-container -->
+
+			<?php endif; ?>
+
+		</div>
+
+	</section> <!-- stat-modules -->
+
 	<section class="band band--thick">
 
 		<div class="wrapper">
@@ -61,148 +91,7 @@
 
 	</section>
 
-	<section class="homepage-contracting / band band--thick / island island--thick island--dark">
-
-		<div class="wrapper">
-
-			<div class="homepage-title">
-				<h1><?php pll_e('Public contracting'); ?></h1>
-				<h2><?php pll_e('The number one corruption risk in government'); ?></h2>
-			</div>
-
-			<?php if ( have_rows('statistics') ) : ?>
-
-				<div class="stat__container">
-
-					<?php while ( have_rows('statistics') ) : the_row(); ?>
-
-						<div class="stat-module">
-							<span><?php the_sub_field('stat'); ?></span>
-							<p><?php the_sub_field('copy'); ?></p>
-						</div>
-
-					<?php endwhile; ?>
-
-				</div> <!-- / .stat-container -->
-
-			<?php endif; ?>
-
-		</div>
-
-	</section> <!-- stat-modules -->
-
 	<div class="wrapper">
-
-		<div class="band band--thicker">
-
-			<div class="homepage-blogs">
-
-				<section>
-
-					<h3 class="border-top"><?php pll_e('Recent Blogs'); ?></h3>
-
-					<?php
-
-						$recent_posts = new query_loop([
-							'post_type' => 'post',
-							'posts_per_page' => 3
-						]);
-
-					?>
-
-					<?php foreach ( $recent_posts as $recent_post ) : ?>
-
-						<a href="<?php the_permalink(); ?>" class="post-object post-object--horizontal / media">
-
-							<div href="#" class="post-object__media / media__object">
-								<?php the_post_thumbnail('hemo_horizontal'); ?>
-							</div>
-
-							<div class="post-object__content / media__body">
-								<div>
-									<h4><?php the_title(); ?></h4>
-								</div>
-								<p>By <?php the_authors(); ?></p>
-							</div>
-
-						</a>
-
-					<?php endforeach; ?>
-
-				</section>
-
-			</div> <!-- homepage-blogs -->
-
-			<div class="homepage-spotlight">
-
-				<section>
-
-					<h3 class="border-top"><?php pll_e('Spotlight'); ?></h3>
-
-					<a href="#" class="post-object post-object--vertical post-object--spotlight">
-
-						<?php if ( have_rows('spotlight') ) : while( have_rows('spotlight') ) : the_row(); ?>
-
-							<div class="post-object__media">
-
-								<img src="http://unsplash.it/459/235" alt="">
-
-								<div class="post-object--spotlight__title">
-									<span class="post-object__tag">Projects</span>
-
-									<?php if ( get_sub_field('title') ) : ?>
-										<h4><?php the_sub_field('title') ?></h4>
-									<?php else : ?>
-										<h4><?php pll_e('Help us define our openness'); ?></h4>
-									<?php endif; ?>
-
-								</div>
-
-							</div>
-
-							<div class="post-object__content">
-
-								<?php if ( get_sub_field('description') ) : ?>
-									<p><?php the_sub_field('description') ?></p>
-								<?php else : ?>
-									<p>Open is part of our name and game at the Open Contracting Partnership. This means, open is at the core of our institution. It is our mission. It is one of our core values. If we want public procurement to be open, we have to be open ourselves.</p>
-								<?php endif; ?>
-
-							</div>
-
-						<?php endwhile; endif; ?>
-
-					</a>
-
-				</section>
-
-			</div> <!-- homepage-spotlight -->
-
-			<?php if ( have_rows('implement_links') ) : ?>
-
-				<div class="homepage-implement">
-
-					<section>
-
-						<div class="heading-highlight">
-							<h3><?php pll_e('Implement Open Contracting'); ?></h3>
-						</div>
-
-						<span><?php pll_e('Learn how to implement open contracting or use contracting data.'); ?></span>
-
-						<ul class="cta-list">
-							<?php while ( have_rows('implement_links') ) : the_row(); ?>
-								<li><a href="<?php the_sub_field('link_address'); ?>"><?php the_sub_field('link_title'); ?></a></li>
-							<?php endwhile; ?>
-						</ul>
-
-					</section>
-
-				</div> <!-- homepage-implement -->
-
-			<?php endif; ?>
-
-		</div>
 
 		<section class="homepage-filter / band band--extra-thick">
 
