cd085c41a32a32c9b442cdc4a5a613abc20b6417
Ben Everard <hello@beneverard.co.uk>
2016-02-24 10:03:41 +0000

    Initial output of advisory board

---
+++ page-advisory-board.php
@@ -0,0 +1,79 @@
+<?php // page.php ?>
+
+<?php get_header(); ?>
+
+	<?php the_post(); ?>
+
+	<div class="wrapper / page__container page--padding">
+
+		<div class="page-advisory-board__intro">
+
+			<h1><?php the_title(); ?></h1>
+
+			<blockquote>
+				<?php the_content(); ?>
+			</blockquote>
+
+		</div>
+
+		<div class="page-advisory-board__minutes">
+
+			<h3>Download minutes from the Advisory Board meetings:</h3>
+
+			<div class="timeline">
+
+				<div class="timeline-item">
+					<span class="timeline-item__date">19-11-2014</span>
+					<p>1st Advisory <br />Board Meeting</p>
+				</div>
+
+				<div class="timeline-item">
+					<span class="timeline-item__date">19-11-2014</span>
+					<p>1st Advisory <br />Board Meeting</p>
+				</div>
+
+				<div class="timeline-item">
+					<span class="timeline-item__date">19-11-2014</span>
+					<p>1st Advisory <br />Board Meeting</p>
+				</div>
+
+				<div class="timeline-item">
+					<span class="timeline-item__date">19-11-2014</span>
+					<p>1st Advisory <br />Board Meeting</p>
+				</div>
+
+				<div class="timeline-item">
+					<span class="timeline-item__date">19-11-2014</span>
+					<p>1st Advisory <br />Board Meeting</p>
+				</div>
+
+			</div>
+
+		</div>
+
+		<article class="page-team__content">
+
+			<?php if ( have_rows('team_members') ) : ?>
+
+				<?php while ( have_rows('team_members') ) : the_row(); ?>
+
+					<div class="team-member" id="<?php echo sanitize_title(get_sub_field('name')); ?>">
+
+						<div class="team-member__image">
+							<img src="http://placehold.it/926x395" />
+						</div>
+
+						<h3><?php the_sub_field('name'); ?></h3>
+						<?php the_sub_field('bio'); ?>
+
+					</div>
+
+				<?php endwhile; ?>
+
+			<?php endif; ?>
+
+		</article>
+
+	</div> <!-- / .wrapper -->
+
+<?php get_footer(); ?>
