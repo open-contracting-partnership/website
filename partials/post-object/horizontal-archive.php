3a8dba4c7b9f95c1ca82956ab0796c152dd56680
Connor Holyday <connorholyday@gmail.com>
2016-02-25 15:08:26 +0000

    Archive post object

---
+++ partials/post-object/horizontal-archive.php
@@ -0,0 +1,23 @@
+<?php $post_type = get_post_type(); ?>
+
+<div class="post-object post-object--archive">
+
+	<div class="post-object__media">
+		<svg><use xlink:href="#icon-<?php echo $post_type; ?>"></svg>
+	</div>
+
+	<div class="post-object__content">
+
+		<div>
+			<span class="tag tag--<?php echo $post_type; ?>"><?php the_post_type_label(); ?></span>
+			<a href="<?php the_permalink(); ?>"><h4><?php the_title(); ?></h4></a>
+		</div>
+
+		<div class="post-object__meta">
+			<span><?php the_authors(FALSE); ?></span>
+			<time>29/01/2016</time>
+		</div>
+
+	</div>
+
+</div>
