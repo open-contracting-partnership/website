ecbb7bb51ec079e7128bf65ba2468573a383b2a7
Connor Holyday <connorholyday@gmail.com>
2016-02-25 15:41:09 +0000

    Styled up archive search

--- archive.php
+++ archive.php
@@ -13,11 +13,7 @@
 
 			<div class="archive-filtering__sort">
 
-				<select class="" name="">
-					<option>All</option>
-				</select>
-
-				<h3>Sort by category</h3>
+				<h4>Sort by category</h4>
 
 				<ul class="nav nav--vertical nav--in-page">
 					<li><a href="#">All</a></li>
@@ -33,9 +29,13 @@
 
 			<h1><?php single_tag_title(); ?></h1>
 
-			<?php if ( have_posts() ) : while(have_posts()) : the_post(); ?>
-				<?php get_partial('post-object', 'horizontal-archive'); ?>
-			<?php endwhile; endif; ?>
+			<div class="archive-content__posts">
+
+				<?php if ( have_posts() ) : while(have_posts()) : the_post(); ?>
+					<?php get_partial('post-object', 'horizontal-archive'); ?>
+				<?php endwhile; endif; ?>
+
+			</div>
 
 		</div>
 
