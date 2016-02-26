81a4dd0515e94d1c3619f123bc75a9124ccc8ed2
Ben Everard <hello@beneverard.co.uk>
2016-02-26 11:18:12 +0000

    Simplified page section functionality

--- page.php
+++ page.php
@@ -19,13 +19,7 @@
 			<article class="page-content cf">
 
 				<div class="blog__title">
-
-					<?php if ( $sections = get_sections() ) : ?>
-						<h1 id="<?php echo key($sections); ?>"><?php echo current($sections); ?></h1>
-					<?php else : ?>
-						<h1><?php the_title(); ?></h1>
-					<?php endif; ?>
-
+					<h1><?php the_title(); ?></h1>
 				</div>
 
 				<section>
