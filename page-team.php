b60decc48984c2d869598de042fed8612e387948
Ben Everard <hello@beneverard.co.uk>
2016-02-27 15:23:44 +0000

    Improved team templaye layout

--- page-team.php
+++ page-team.php
@@ -6,13 +6,17 @@
 
 	<div class="wrapper / page__container">
 
-		<aside class="page-team__sidebar">
+		<aside class="page-team__title">
 
 			<h1><?php the_title(); ?></h1>
 
 			<blockquote>
 				<?php the_content(); ?>
 			</blockquote>
+
+		</aside>
+
+		<aside class="page-team__sidebar">
 
 			<?php if ( have_rows('team_members') ) : ?>
 
@@ -38,12 +42,23 @@
 
 					<div class="team-member" id="<?php echo sanitize_title(get_sub_field('name')); ?>">
 
-						<div class="team-member__image">
-							<img src="http://placehold.it/926x395" />
+						<?php if ( $avatar = get_sub_field('image') ) : ?>
+
+							<div class="team-member__avatar">
+								<img src="<?php echo $avatar['sizes']['thumbnail']; ?>" />
+							</div>
+
+						<?php endif; ?>
+
+						<div class="team-member__name">
+							<h3><?php the_sub_field('name'); ?></h3>
+							<a href="#" class="team-member__view-bio">View profile</a>
 						</div>
 
-						<h3><?php the_sub_field('name'); ?></h3>
-						<?php the_sub_field('bio'); ?>
+						<div class="team-member__bio">
+							<?php the_sub_field('bio'); ?>
+						</div>
+
 
 					</div>
 
