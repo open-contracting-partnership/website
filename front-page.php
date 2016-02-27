d537ed0d768acc6af3b6a07ce4579955e13f3fde
Shane Griffiths <shane@theideabureau.co>
2016-02-27 12:27:45 +0000

    Small typo fix for mexico city

--- front-page.php
+++ front-page.php
@@ -188,7 +188,7 @@
 			<div class="homepage-map__image">
 
 				<div class="homepage-map__image-item">
-					<a href="#" class="button button--white">Mexico City</a>
+					<a href="#" class="button button--white">Mexico&nbsp;City</a>
 				</div>
 
 				<div class="homepage-map__image-item">
