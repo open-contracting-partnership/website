0aa59c1a698b5039da52912867037c7d48c84bc7
Connor Holyday <connorholyday@gmail.com>
2016-03-01 11:38:54 +0000

    Restyle of the event object

--- partials/post-object/event.php
+++ partials/post-object/event.php
@@ -1,18 +1,15 @@
 <div class="post-object post-object--horizontal post-object--event">
 
 	<div class="post-object__media">
-		<time><?php the_time('j'); ?><span><?php the_time('S'); ?></span> <em><?php the_time('M'); ?></em></time>
+		<time><?php the_time('j'); ?><span><?php the_time('S'); ?></span><em><?php the_time('M'); ?></em></time>
 	</div>
 
 	<div class="post-object__content">
-
 		<h4><?php the_title(); ?></h4>
-		<?php /* <a href="/events"><h4><?php the_title(); ?></h4></a> */ ?>
+	</div>
 
-		<?php //<span class="event__location">London, UK</span> ?>
-
-		<?php /* <a href="<?php the_permalink(); ?>" class="button button--small button--tag">Details</a> */ ?>
-
+	<div class="post-object__link">
+		<a href="<?php the_permalink(); ?>">Details</a>
 	</div>
 
 </div>
