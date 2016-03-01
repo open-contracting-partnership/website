f2dabd5fe6d9e5657ed1de33b66aa9899a92a9f2
Connor Holyday <connorholyday@gmail.com>
2016-03-01 15:54:31 +0000

    Fixed up events date

--- partials/post-object/event.php
+++ partials/post-object/event.php
@@ -1,7 +1,19 @@
+<?php
+
+	if ( $date = get_field('event_date') ) {
+		$date = strtotime($date);
+	}
+
+?>
+
 <div class="post-object post-object--event">
 
 	<div class="post-object__media">
-		<time><?php the_time('j'); ?><span><?php the_time('S'); ?></span><em><?php the_time('M'); ?></em></time>
+		<?php if ( $date ) : ?>
+			<time><?php echo date('j', $date); ?><span><?php echo date('S', $date); ?></span><em><?php echo date('M', $date); ?></em></time>
+		<?php else : ?>
+			<em>TBA</em>
+		<?php endif; ?>
 	</div>
 
 	<div class="post-object__content">
