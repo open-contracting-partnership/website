ee585601d4577c67b30958548bdca0ee11972f0c
Ben Everard <hello@beneverard.co.uk>
2016-03-02 12:07:53 +0000

    Added hotjar tracking code to footer

--- footer.php
+++ footer.php
@@ -165,6 +165,18 @@
 
 		<?php wp_footer(); ?>
 
+		<!-- Hotjar Tracking Code for Open Contracting Partnership -->
+		<script>
+		    (function(h,o,t,j,a,r){
+		        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+		        h._hjSettings={hjid:109763,hjsv:5};
+		        a=o.getElementsByTagName('head')[0];
+		        r=o.createElement('script');r.async=1;
+		        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+		        a.appendChild(r);
+		    })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+		</script>
+
 	</body>
 
 </html>
