d89e9e3d7347667f26f1869d210f4b545e80dad2
Ben Everard <hello@beneverard.co.uk>
2016-03-04 16:16:20 +0000

    Added typekit to ds map

--- endpoints/map.php
+++ endpoints/map.php
@@ -1,3 +1,6 @@
+<script src="https://use.typekit.net/xpw3jps.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+
 <link rel="stylesheet" href="/wp-content/themes/ocp-v1/assets/map/styles/main-9f505b5fc2.css">
 <script src="/wp-content/themes/ocp-v1/assets/map/scripts/vendor-f502083317.js"></script>
 <script src="/wp-content/themes/ocp-v1/assets/map/scripts/bundle-c3c893c47f.js"></script>
