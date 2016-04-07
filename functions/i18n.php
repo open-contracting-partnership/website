4927b2290800fde11d7a7bf5f34e68a698e84fdc
Ben Everard <hello@beneverard.co.uk>
2016-04-07 13:22:13 +0100

    Improved query_loop method to better handle multilingual content

--- functions/i18n.php
+++ functions/i18n.php
@@ -23,3 +23,74 @@ add_filter('pll_get_post_types', function($types) {
 	unset ($types['post']);
 	return $types;
 });
+
+
+ //**************************
+// WPML TRANSLATION HANDLING
+
+// WPML doesn't play very well with custom queries and fetching a mix of
+// languages, as such filter query_loop to allow for this mix
+
+$current_lang = '';
+
+add_action('query_loop_pre_query', function() {
+
+	global $sitepress, $current_lang;
+
+	// only continue to filter if the WPML plugin is enabled
+	if ( isset($sitepress) ) {
+
+		$current_lang = $sitepress->get_current_language();
+		$default_lang = $sitepress->get_default_language();
+		$diff_lang = $current_lang !== $default_lang;
+
+		if ( $diff_lang ) {
+			$sitepress->switch_lang($default_lang);
+		}
+
+	}
+
+});
+
+add_filter('query_loop_post_query', function($posts) {
+
+	global $sitepress, $current_lang;
+
+	$translated_posts = array();
+
+	// only continue to filter if the WPML plugin is enabled
+	if ( isset($sitepress) ) {
+
+		$default_lang = $sitepress->get_default_language();
+		$diff_lang = $current_lang !== $default_lang;
+
+		if ( $diff_lang ) {
+
+			// loop through the default posts, translating any posts back
+			// to the current language where possible
+
+			foreach ( $posts as $post ) {
+
+				$translated_id = icl_object_id($post->ID, $post->post_type, FALSE, $current_lang);
+
+				if ( $translated_id ) {
+					$translated_posts[] = get_post($translated_id);
+				} else {
+					$translated_posts[] = $post;
+				}
+
+
+			}
+
+			$posts = $translated_posts;
+
+			// revert the language back to the original
+			$sitepress->switch_lang($current_lang);
+
+		}
+
+	}
+
+	return $posts;
+
+});
--- functions/query_loop.php
+++ functions/query_loop.php
@@ -8,18 +8,24 @@
  * - Can be used within a foreach
  * - Can be counted within count()
  * - Automatically calls have_posts() and the_post() on each iteration
- * 
+ *
  * @param  array $args
  * @return array
  */
 class query_loop implements Iterator, Countable {
 
 	public $post_ids = array();
-	
+
 	public function __construct( $args = array() ) {
+
+		// fire off the pre query filter
+		do_action('query_loop_pre_query');
 
 		// init WP_Query
 		$this->query = new WP_Query($args);
+
+		// fire off the post query filter
+		$this->query->posts = apply_filters('query_loop_post_query', $this->query->posts);
 
 		// compile a list of post_ids
 		foreach ( $this->query->posts as $post ) {
@@ -31,24 +37,24 @@ class query_loop implements Iterator, Countable {
 	function count() {
 		return count($this->query->posts);
 	}
- 
+
 	function rewind() {
 		$this->query->rewind_posts();
 	}
- 
+
 	function current() {
 		$this->query->the_post();
 		return $this->query->post;
 	}
- 
+
 	function key() {
 		return $this->query->post->ID;
 	}
- 
+
 	function next() {
 		//$this->query->the_post();
 	}
- 
+
 	function valid() {
 		if ( $this->query->have_posts() ) {
 			return true;
