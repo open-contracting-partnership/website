26b7906fa3117b80045470f47d370ce735896045
Connor Holyday <connorholyday@gmail.com>
2017-06-22 15:02:53 +0100

    Added styleguide to gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -192,7 +192,7 @@ gulp.task('watch', function () {
 	livereload.listen();
 
 	// boilerplate
-	gulp.watch('assets/scss/**/*.scss', ['scss', 'scss-lint', 'modernizr']);
+	gulp.watch('assets/scss/**/*.scss', ['scss', 'scss-lint', 'styleguide']);
 	gulp.watch('assets/js/main.js', ['js', 'modernizr']);
 
 });
