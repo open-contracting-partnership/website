1c593bf57fb9d9f907fa1d0224cb832a00a98279
Ben Everard <hello@beneverard.co.uk>
2017-08-09 13:55:58 +0100

    Added social links to `.update-panel` component

--- partials/update/panel.php
+++ partials/update/panel.php
@@ -21,9 +21,19 @@
 		<p>Follow up and keep up to date</p>
 
 		<ul class="nav nav--horizontal">
-			<li><a href="#" target="_blank"><svg><use xlink:href="#icon-facebook"></svg></a></li>
-			<li><a href="#" target="_blank"><svg><use xlink:href="#icon-twitter"></svg></a></li>
-			<li><a href="#" target="_blank"><svg><use xlink:href="#icon-linkedin"></svg></a></li>
+
+			<?php if ( $twitter_url = get_field('twitter_url', 'options') ) : ?>
+				<li><a href="<?php echo $twitter_url; ?>" target="_blank"><svg><use xlink:href="#icon-twitter" /></svg></a></li>
+			<?php endif; ?>
+
+			<?php if ( $facebook_url = get_field('facebook_url', 'options') ) : ?>
+				<li><a href="<?php echo $facebook_url; ?>" target="_blank"><svg><use xlink:href="#icon-facebook" /></svg></a></li>
+			<?php endif; ?>
+
+			<?php if ( $linkedin_url = get_field('linkedin_url', 'options') ) : ?>
+				<li><a href="<?php echo $linkedin_url; ?>" target="_blank"><svg><use xlink:href="#icon-linkedin" /></svg></a></li>
+			<?php endif; ?>
+
 		</ul>
 
 	</div>
