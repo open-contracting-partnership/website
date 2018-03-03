772496b62886879e3b7ed4665b5858b682e85cbd
Ben Everard <hello@beneverard.co.uk>
2018-03-03 10:41:38 +0000

    Added Laravel mix

---
+++ webpack.mix.js
@@ -0,0 +1,40 @@
+let mix = require('laravel-mix');
+var path = require('path');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.sass('resources/scss/styles.scss', 'dist/css')
+	.js('resources/js/main.js', 'dist/js')
+	.js('resources/js/impact-stories.js', 'dist/js')
+	.js('resources/js/get-started.js', 'dist/js')
+	.js('resources/js/worldwide.js', 'dist/js')
+	.options({
+		processCssUrls: false
+	})
+	.webpackConfig({
+		resolve: {
+			modules: [
+				'node_modules'
+			],
+			alias: {
+				'vue$': 'vue/dist/vue.js'
+			}
+		},
+		module: {
+			noParse: /(mapbox-gl)\.js$/,
+			rules: [{
+				test: /\.scss/,
+				enforce: 'pre',
+				loader: 'import-glob-loader'
+			}]
+		}
+	});
