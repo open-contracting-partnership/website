190bf5ea8eda162eed78bf0bd67be8ce6255dc60
Ben Everard <hello@beneverard.co.uk>
2018-03-07 14:03:49 +0000

    Centre world map

--- resources/js/components/worldwide/map.vue
+++ resources/js/components/worldwide/map.vue
@@ -350,7 +350,6 @@
 			filters: {
 
 				handler() {
-				console.log(this.map.getBounds());
 					this.setMapStyles();
 				},
 
@@ -368,7 +367,7 @@
 				this.map = new mapboxgl.Map({
 					container: 'map',
 					style: 'mapbox://styles/theideabureau/cj98q867x2m1x2slgf3d860hl',
-					zoom: 1
+					zoom: 1.3
 				});
 
 				this.map.once('load', function() {
@@ -376,49 +375,22 @@
 					// mark the map as being loaded
 					this.map_loaded = true;
 
-
-
-console.log(this.map.getBounds());
-
-//
-//
-//
-// lat
-// :
-// 71.13098770917262
-// lng
-// :
-// 225.00000000001478
-// __proto__
-// :
-// Object
-// _sw
-// :
-// LngLat
-// lat
-// :
-// -71.13098770916976
-// lng
-// :
-// -225.00000000000603
-//
-
-					const padding = 0
-					let left_padding = padding;
-
 					if ( window.innerWidth >= 1024 ) {
-						left_padding += 385;
+
+						const padding = 0
+						let left_padding = 385;
+
+						// fit the map to the bounds of the available countries
+						this.map.fitBounds(this.map.getBounds(), {
+							padding: {
+								top: padding + 200,
+								right: padding,
+								bottom: padding,
+								left: left_padding,
+							}
+						});
+
 					}
-
-					// fit the map to the bounds of the available countries
-					this.map.fitBounds([
-						[-180, 85],
-						[180, -85]
-					]);
-
-
-
-
 
 				}.bind(this));
 
