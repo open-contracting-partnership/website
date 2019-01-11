7721f65ed690c4ad5d3f34b1a27baf9df3ac09b8
Ben Everard <hello@beneverard.co.uk>
2019-01-11 10:20:20 +0000

    Added sentry PHP tracking

--- functions/loader.php
+++ functions/loader.php
@@ -7,6 +7,17 @@
 require(get_theme_root() . '/' . get_template() . '/vendor/autoload.php');
 
 
+ //***********************
+// SENTRY ERROR REPORTING
+
+$sentryClient = new Raven_Client('https://83ad0f87126d451bb12da818e5c7e46d@sentry.io/1369375');
+
+$error_handler = new Raven_ErrorHandler($sentryClient);
+$error_handler->registerExceptionHandler();
+$error_handler->registerErrorHandler();
+$error_handler->registerShutdownFunction();
+
+
  //************************
 // THEME PLUGINS DIRECTORY
 
