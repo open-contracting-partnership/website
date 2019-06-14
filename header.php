a1f9fdc72a7c8d237b2a374ed23c532a7239ad6f
Ben Everard <hello@beneverard.co.uk>
2019-06-14 09:25:36 +0100

    Updated favicon

--- header.php
+++ header.php
@@ -18,27 +18,13 @@
 		<meta name="MobileOptimized" content="320">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-		<link rel="apple-touch-icon" sizes="57x57" href="<?php bloginfo('template_directory'); ?>/resources/img/favicons/apple-touch-icon-57x57.png">
-		<link rel="apple-touch-icon" sizes="60x60" href="<?php bloginfo('template_directory'); ?>/resources/img/favicons/apple-touch-icon-60x60.png">
-		<link rel="apple-touch-icon" sizes="72x72" href="<?php bloginfo('template_directory'); ?>/resources/img/favicons/apple-touch-icon-72x72.png">
-		<link rel="apple-touch-icon" sizes="76x76" href="<?php bloginfo('template_directory'); ?>/resources/img/favicons/apple-touch-icon-76x76.png">
-		<link rel="apple-touch-icon" sizes="114x114" href="<?php bloginfo('template_directory'); ?>/resources/img/favicons/apple-touch-icon-114x114.png">
-		<link rel="apple-touch-icon" sizes="120x120" href="<?php bloginfo('template_directory'); ?>/resources/img/favicons/apple-touch-icon-120x120.png">
-		<link rel="apple-touch-icon" sizes="144x144" href="<?php bloginfo('template_directory'); ?>/resources/img/favicons/apple-touch-icon-144x144.png">
-		<link rel="apple-touch-icon" sizes="152x152" href="<?php bloginfo('template_directory'); ?>/resources/img/favicons/apple-touch-icon-152x152.png">
-		<link rel="apple-touch-icon" sizes="180x180" href="<?php bloginfo('template_directory'); ?>/resources/img/favicons/apple-touch-icon-180x180.png">
-		<link rel="icon" type="image/png" href="<?php bloginfo('template_directory'); ?>/resources/img/favicons/favicon-32x32.png" sizes="32x32">
-		<link rel="icon" type="image/png" href="<?php bloginfo('template_directory'); ?>/resources/img/favicons/favicon-194x194.png" sizes="194x194">
-		<link rel="icon" type="image/png" href="<?php bloginfo('template_directory'); ?>/resources/img/favicons/favicon-96x96.png" sizes="96x96">
-		<link rel="icon" type="image/png" href="<?php bloginfo('template_directory'); ?>/resources/img/favicons/android-chrome-192x192.png" sizes="192x192">
-		<link rel="icon" type="image/png" href="<?php bloginfo('template_directory'); ?>/resources/img/favicons/favicon-16x16.png" sizes="16x16">
-		<link rel="manifest" href="<?php bloginfo('template_directory'); ?>/resources/img/favicons/manifest.json">
-		<link rel="mask-icon" href="<?php bloginfo('template_directory'); ?>/resources/img/favicons/safari-pinned-tab.svg" color="#323238">
-		<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/resources/img/favicons/favicon.ico">
-		<meta name="msapplication-TileColor" content="#323238">
-		<meta name="msapplication-TileImage" content="<?php bloginfo('template_directory'); ?>/resources/img/favicons/mstile-144x144.png">
-		<meta name="msapplication-config" content="<?php bloginfo('template_directory'); ?>/resources/img/favicons/browserconfig.xml">
-		<meta name="theme-color" content="#323238">
+		<link rel="apple-touch-icon" sizes="180x180" href="<?php bloginfo('template_directory'); ?>/resources/img/favicons/apple-touch-icon.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="<?php bloginfo('template_directory'); ?>/resources/img/favicons/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="<?php bloginfo('template_directory'); ?>/resources/img/favicons/favicon-16x16.png">
+		<link rel="manifest" href="<?php bloginfo('template_directory'); ?>/resources/img/favicons/site.webmanifest">
+		<link rel="mask-icon" href="<?php bloginfo('template_directory'); ?>/resources/img/favicons/safari-pinned-tab.svg" color="#4d4d54">
+		<meta name="msapplication-TileColor" content="#4d4d54">
+		<meta name="theme-color" content="#ffffff">
 
 		<?php wp_head(); ?>
 
