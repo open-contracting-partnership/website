2dbd1bae5f8eb6f8c71dada776c008447001f4f6
Ben Everard <hello@beneverard.co.uk>
2019-06-14 09:27:06 +0100

    Added cache busting to favicon

--- header.php
+++ header.php
@@ -18,11 +18,11 @@
 		<meta name="MobileOptimized" content="320">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-		<link rel="apple-touch-icon" sizes="180x180" href="<?php bloginfo('template_directory'); ?>/resources/img/favicons/apple-touch-icon.png">
-		<link rel="icon" type="image/png" sizes="32x32" href="<?php bloginfo('template_directory'); ?>/resources/img/favicons/favicon-32x32.png">
-		<link rel="icon" type="image/png" sizes="16x16" href="<?php bloginfo('template_directory'); ?>/resources/img/favicons/favicon-16x16.png">
-		<link rel="manifest" href="<?php bloginfo('template_directory'); ?>/resources/img/favicons/site.webmanifest">
-		<link rel="mask-icon" href="<?php bloginfo('template_directory'); ?>/resources/img/favicons/safari-pinned-tab.svg" color="#4d4d54">
+		<link rel="apple-touch-icon" sizes="180x180" href="<?php bloginfo('template_directory'); ?>/resources/img/favicons/apple-touch-icon.png?v=2">
+		<link rel="icon" type="image/png" sizes="32x32" href="<?php bloginfo('template_directory'); ?>/resources/img/favicons/favicon-32x32.png?v=2">
+		<link rel="icon" type="image/png" sizes="16x16" href="<?php bloginfo('template_directory'); ?>/resources/img/favicons/favicon-16x16.png?v=2">
+		<link rel="manifest" href="<?php bloginfo('template_directory'); ?>/resources/img/favicons/site.webmanifest?v=2">
+		<link rel="mask-icon" href="<?php bloginfo('template_directory'); ?>/resources/img/favicons/safari-pinned-tab.svg?v=2" color="#4d4d54">
 		<meta name="msapplication-TileColor" content="#4d4d54">
 		<meta name="theme-color" content="#ffffff">
 
