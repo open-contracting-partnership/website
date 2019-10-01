d2be205c87244a1774c17b5dfe6f564361551e5a
Ben Everard <hello@beneverard.co.uk>
2019-10-01 13:28:28 +0100

    Wipes the repo clean, ahead of the new redesign
    Files that are re-introduced later will retain their file history, so long as the name remains them same.

--- partials/page/strips.php
+++
@@ -1,9 +0,0 @@
-<?php // partials/page/strips.php ?>
-
-<?php if ( have_rows('page_strips') ) : ?>
-
-	<?php while ( have_rows('page_strips') ) : the_row(); ?>
-		<?php get_partial('page', 'strip'); ?>
-	<?php endwhile; ?>
-
-<?php endif; ?>
--- resources/js/svg.js
+++
@@ -1,4 +0,0 @@
-var __svgstore__ = {
-	path: '../svg/*.svg',
-	name: 'dist/svg/icons.svg'
-};
