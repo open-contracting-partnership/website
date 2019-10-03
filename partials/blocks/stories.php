db4d7bcfd9c46f384cd67138b000bd0be6d48ef5
Ben Everard <hello@beneverard.co.uk>
2019-10-03 09:47:00 +0100

    Output more components within storied block

--- partials/blocks/stories.php
+++ partials/blocks/stories.php
@@ -2,8 +2,12 @@
 
 	global $post;
 
-	$title = get_field('title') ?: 'Add your title here&hellip;';
+	$primary_title = get_field('primary_title') ?: 'Add primary title here&hellip;';
 	$primary_posts = get_field('primary_stories');
+
+	$secondary_title = get_field('secondary_title') ?: 'Add secondary title here&hellip;';
+	$secondary_posts = get_field('secondary_stories');
+
 	$background_colour = get_field('background_colour') ?: '#6C75E1';
 	$text_colour = isContrastingColourLight($background_colour) ? '#FFF' : '#000';
 	$text_colour = get_field('text_colour') ?: $text_colour;
@@ -12,7 +16,7 @@
 
 <div class="block block--stories" style="color: <?php echo $text_colour; ?>; background-color: <?php echo $background_colour; ?>">
 
-	<h1 class="block__heading"><?php echo $title; ?></h1>
+	<h1 class="block__heading"><?php echo $primary_title; ?></h1>
 
 	<?php if ( $primary_posts ) : ?>
 
@@ -21,6 +25,21 @@
 			<?php setup_postdata($post = $post_id); ?>
 
 			<?php the_partial('card', 'image'); ?>
+
+		<?php endforeach; /* and */ wp_reset_postdata(); ?>
+
+	<?php endif; ?>
+
+
+	<h2 class="block__heading"><?php echo $secondary_title; ?></h2>
+
+	<?php if ( $secondary_posts ) : ?>
+
+		<?php foreach ( $secondary_posts as $post_data ) : ?>
+
+			<?php setup_postdata($post = $post_data['post']); ?>
+
+			<?php the_partial('card', 'primary'); ?>
 
 		<?php endforeach; /* and */ wp_reset_postdata(); ?>
 
