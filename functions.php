c9cc60ac9c1b4f0ae2972134783771c46e28346b
Ben Everard <hello@beneverard.co.uk>
2019-10-14 16:33:02 +0100

    Added WP header implementation

--- functions.php
+++ functions.php
@@ -18,4 +18,32 @@ require_once('routes.php');
 require_once('functions/loader.php');
 
 // Set global params in the Timber context
-add_filter('timber_context', [$lumberjack, 'addToContext']);
+add_filter('timber/context', [$lumberjack, 'addToContext']);
+
+// set header and footer
+add_filter('timber/context', function($context) {
+
+	$context['header_primary_menu'] = new \Timber\Menu('Header: Primary');
+	$context['header_secondary_menu'] = new \Timber\Menu('Header: Secondary');
+	$context['search_term'] = get_search_query();
+
+	// fetch the menu
+	$mega_menus = get_field('mega_menu', 'options');
+
+	foreach ( $context['header_primary_menu']->items as &$item ) {
+
+		$mega_menu = array_filter($mega_menus, function($menu) use ($item){
+			return $menu['parent'] == $item->id;
+		});
+
+		$item->mega_menu = false;
+
+		if ( $mega_menu ) {
+			$item->mega_menu = $mega_menu[0];
+		}
+
+	}
+
+	return $context;
+
+});
--- functions/assets.php
+++ functions/assets.php
@@ -12,12 +12,16 @@ add_action('wp_enqueue_scripts', function() {
         // REGISTER ASSETS
 
 		Assets::registerStyle('main', '/dist/css/styles.css');
+		Assets::registerScript('main', '/dist/js/scripts.js', [], true);
+		Assets::registerScript('header', '/dist/js/header.js', [], true);
 
 
          //************
         // LOAD ASSETS
 
 		wp_enqueue_style('main');
+		wp_enqueue_script('main');
+		wp_enqueue_script('header');
 
 	}
 
