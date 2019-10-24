6357c79ef665a07afa5c5b93134724e981395302
Ben Everard <hello@beneverard.co.uk>
2019-10-24 12:19:29 +0100

    Added "Image with links" block

--- config/app.php
+++ config/app.php
@@ -35,6 +35,7 @@ return [
 		// Block Providers
         App\Providers\Blocks\ContentWithSidebarServiceProvider::class,
         App\Providers\Blocks\CoverServiceProvider::class,
+        App\Providers\Blocks\ImageWithLinksServiceProvider::class,
         App\Providers\Blocks\MoreStoriesServiceProvider::class,
         App\Providers\Blocks\ResourceServiceProvider::class,
         App\Providers\Blocks\StoriesServiceProvider::class,
