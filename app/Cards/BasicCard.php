d61ddeb8a072d7a738b68af8d733cdb8e541f3e3
Ben Everard <ben@theideabureau.co>
2019-11-13 15:48:09 +0000

    Fixed tag type colour

--- app/Cards/BasicCard.php
+++ app/Cards/BasicCard.php
@@ -33,6 +33,7 @@ class BasicCard
 				'title' => $post->post_title,
 				'url' => $post->link,
 				'meta' => $post->date('j M Y'),
+				'type' => $post->post_type,
 				'type_label' => get_post_type_label($post->post_type)
 			];
 
