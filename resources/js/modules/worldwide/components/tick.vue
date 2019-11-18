c35393c9d206ee727a0c36a907c3d8429c4c7de8
Ben Everard <ben@theideabureau.co>
2019-11-18 16:12:25 +0000

    Made input circle it's own component

--- resources/js/modules/worldwide/components/tick.vue
+++ resources/js/modules/worldwide/components/tick.vue
@@ -1,6 +1,6 @@
 <template>
 
-	<div class="tick" :data-colour="colour" :checked="checked" role="checkbox" :aria-checked="checked" v-bind:class="{
+	<div class="tick / input-circle" :data-colour="colour" :checked="checked" role="checkbox" :aria-checked="checked" v-bind:class="{
 			'checked': checked,
 		}">
 	</div>
@@ -14,41 +14,3 @@
 	}
 
 </script>
-
-<style lang="scss">
-
-	// ATTN: this is not ideal, but webpack aliases aren't working
-	@import "../../../../scss/_bootstrap.scss";
-
-	.tick {
-
-		--colour: #{$ui-grey-4};
-
-		font-size: 21px;
-		border: 3px solid var(--colour);
-		background-color: $ui-white;
-		width: 1em;
-		height: 1em;
-		cursor: pointer;
-		user-select: none;
-		border-radius: 50%;
-
-		&.checked {
-			background-color: var(--colour);
-		}
-
-		&[data-colour="blue"] {
-			--colour: #{$ui-blue};
-		}
-
-		&[data-colour="teal"] {
-			--colour: #{$ui-teal};
-		}
-
-		&[data-colour="light-green"] {
-			--colour: #{$ui-brand-2};
-		}
-
-	}
-
-</style>
