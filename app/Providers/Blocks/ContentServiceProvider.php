f670908dbb0dfc8bce8ad81d71c0619084eed784
Ben Everard <ben@theideabureau.co>
2019-11-20 14:49:38 +0000

    Further improvements to implement page

--- app/Providers/Blocks/ContentServiceProvider.php
+++ app/Providers/Blocks/ContentServiceProvider.php
@@ -41,11 +41,15 @@ class ContentServiceProvider
 		$context['block'] = [];
 		$context['block']['title'] = get_field('title');
 		$context['block']['content'] = get_field('content');
+
 		$context['block']['background_colour'] = get_field('background_colour') ?: '#FFFFFF';
 		$context['block']['is_dark'] = isContrastingColourLight($context['block']['background_colour']);
 		$context['block']['text_colour'] = $context['block']['is_dark'] ? '#FFF' : '#000';
 		$context['block']['text_colour'] = get_field('text_colour') ?: $context['block']['text_colour'];
+
 		$context['block']['width'] = get_field('width') ?: 'full';
+		$context['block']['alignment'] = get_field('alignment') ?: 'left';
+		$context['block']['size'] = get_field('size') ?: 'normal';
 
 		echo Timber::compile('blocks/content.twig', $context);
 
