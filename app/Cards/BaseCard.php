0332530623b448e12f1df0499450605f425139b0
Ben Everard <ben@theideabureau.co>
2019-11-22 12:38:03 +0000

    Improved base card convert post functionality

--- app/Cards/BaseCard.php
+++ app/Cards/BaseCard.php
@@ -6,17 +6,15 @@ class BaseCard
 {
 
 	public static function convertPost($post) {
-		
+
 		if ( is_int($post) ) {
 			return static::buildPostById($post);
-		}
-
-		if ( is_object($post) && get_class($post) === 'WP_Post' ) {
+		} else if ( is_object($post) && get_class($post) === 'WP_Post' ) {
 			return static::buildPostById($post->ID);
-		}
-
-		if ( is_object($post) && ( get_class($post) === 'Timber\Post' || is_subclass_of($post, 'Timber\Post') ) ) {
+		} else if ( is_object($post) && ( get_class($post) === 'Timber\Post' || is_subclass_of($post, 'Timber\Post') ) ) {
 			return static::convertTimberPost($post);
+		} else if ( method_exists(static::class, 'buildData') ) {
+			return static::buildData($post);
 		}
 
 	}
