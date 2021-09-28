710a0ae34f7fa3bd4bea30d203c3909d893e6968
Tom Pinkett <tom@theideabureau.co>
2021-09-28 11:10:05 +0100

    Resource single update progress

--- single-resource.php
+++ single-resource.php
@@ -20,6 +20,8 @@ class SingleResourceController extends Controller
 		$context = Timber::get_context();
 		$resource = new Resource();
 
+		// dd($resource);
+
 		$context['resource'] = [];
 
 		$context['resource']['title'] = $resource->title;
@@ -29,17 +31,20 @@ class SingleResourceController extends Controller
 
 		if ( $context['resource']['type'] ) {
 			$context['resource']['type'] = get_term($context['resource']['type']);
+			$context['resource']['colour'] = get_field('colour', $context['resource']['type']);
 		}
 
 		$context['resource']['attachments'] = get_field('attachments', $resource->ID);
 		$context['resource']['link'] = get_field('link', $resource->ID);
 
 		if ( $resource->organisation ) {
+			$context['resource']['organisation'] = $resource->organisation;
 			$context['resource']['meta'] = sprintf(__('By %s', 'ocp'), $resource->organisation) . ' / ' . $resource->date('Y');
 		} else {
 			$context['resource']['meta'] = $resource->date('Y');
 		}
 
+		$context['resource']['published'] = $resource->date('Y');
 
 		// terms
 		$context['resource']['taxonomies']['issue'] = $resource->issue;
--- webpack.mix.js
+++ webpack.mix.js
@@ -33,60 +33,60 @@ if ( ! mix.inProduction() ) {
 
 }
 
-mix.svgSprite('resources/svg', 'svg/icons.svg')
+// mix.svgSprite('resources/svg', 'svg/icons.svg')
 
 mix.sass('resources/scss/styles.scss', 'dist/css')
 	.sass('resources/scss/gutenberg.scss', 'dist/css')
-	.sass('styleguide_assets/aigis_assets/styles/theme.scss', 'styleguide')
+	// .sass('styleguide_assets/aigis_assets/styles/theme.scss', 'styleguide')
 	.options({
 		processCssUrls: false,
-        postCss: [tailwindcss('./tailwind.config.js')],
+        // postCss: [tailwindcss('./tailwind.config.js')],
 	});
 
 mix.js('resources/js/scripts.js', 'dist/js')
-	.js('resources/js/element-queries.js', 'dist/js')
-	.js('resources/js/block-download-carousel.js', 'dist/js')
-	.js('resources/js/block-featured-stories-carousel.js', 'dist/js')
-	.js('resources/js/block-our-model.js', 'dist/js')
-	.js('resources/js/block-quote-carousel.js', 'dist/js')
-	.js('resources/js/block-team-profile.js', 'dist/js')
-	.js('resources/js/card-quote.js', 'dist/js')
-	.js('resources/js/header.js', 'dist/js')
-	.js('resources/js/impact-stories.js', 'dist/js')
-	.js('resources/js/latest-news.js', 'dist/js')
-	.js('resources/js/archive-resource.js', 'dist/js')
-	.js('resources/js/mailchimp.js', 'dist/js')
-	.js('resources/js/svg.js', 'dist/js')
-	.js('resources/js/modules/worldwide/worldwide.js', 'dist/js')
-	.extract(['vue'])
-	.then((stats) => {
+	// .js('resources/js/element-queries.js', 'dist/js')
+	// .js('resources/js/block-download-carousel.js', 'dist/js')
+	// .js('resources/js/block-featured-stories-carousel.js', 'dist/js')
+	// .js('resources/js/block-our-model.js', 'dist/js')
+	// .js('resources/js/block-quote-carousel.js', 'dist/js')
+	// .js('resources/js/block-team-profile.js', 'dist/js')
+	// .js('resources/js/card-quote.js', 'dist/js')
+	// .js('resources/js/header.js', 'dist/js')
+	// .js('resources/js/impact-stories.js', 'dist/js')
+	// .js('resources/js/latest-news.js', 'dist/js')
+	// .js('resources/js/archive-resource.js', 'dist/js')
+	// .js('resources/js/mailchimp.js', 'dist/js')
+	// .js('resources/js/svg.js', 'dist/js')
+	// .js('resources/js/modules/worldwide/worldwide.js', 'dist/js')
+	// .extract(['vue'])
+	// .then((stats) => {
 
-		if ( ! mix.inProduction() ) {
+	// 	if ( ! mix.inProduction() ) {
 
-			const files = Object.keys(stats.compilation.assets);
-			const trigger_files = [
-				'/css/styles.css'
-			];
+	// 		const files = Object.keys(stats.compilation.assets);
+	// 		const trigger_files = [
+	// 			'/css/styles.css'
+	// 		];
 
-			// check if any of the trigger files are in the latest compile
-			const diff = files.filter(function(n) {
-			    return trigger_files.indexOf(n) !== -1;
-			});
+	// 		// check if any of the trigger files are in the latest compile
+	// 		const diff = files.filter(function(n) {
+	// 		    return trigger_files.indexOf(n) !== -1;
+	// 		});
 
-			// and if they are, trigger the styleguide
-			if ( diff.length ) {
+	// 		// and if they are, trigger the styleguide
+	// 		if ( diff.length ) {
 
-				setTimeout(() => {
-					const Aigis = require('node-aigis');
-					const aigis = new Aigis('./styleguide_config.yml');
-					aigis.run();
-				}, 100);
+	// 			setTimeout(() => {
+	// 				const Aigis = require('node-aigis');
+	// 				const aigis = new Aigis('./styleguide_config.yml');
+	// 				aigis.run();
+	// 			}, 100);
 
-			}
+	// 		}
 
-		}
+	// 	}
 
-	})
+	// })
 	.webpackConfig({
 		resolve: {
 			modules: [
