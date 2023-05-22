a33555929313dfae98a0ce2e94541780545544e0
Ben Everard <ben@theideabureau.co>
2023-05-22 15:56:31 +0100

    Added stat animation

--- app/Providers/Blocks/StatsServiceProvider.php
+++ app/Providers/Blocks/StatsServiceProvider.php
@@ -17,11 +17,12 @@ class StatsServiceProvider
                 'title' => __('Stats'),
                 'render_callback' => array($this, 'render'),
                 'category' => 'ocp-blocks',
-                // 'icon' => '<svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 128"><path fill="#606" d="M0 0h128v128H0z"/><path fill="#fff" d="M41 59h37.1v10H41zM41 92h45.1v10H41zM41 28h43.1v10H41z"/><path fill="#fff" d="M41 28h11.1v74H41z"/></svg>',
-                // 'mode' => 'preview',
                 'supports' => [
                     'align' => false,
                 ],
+                'enqueue_assets' => function () {
+                    wp_enqueue_script('block-stats', get_template_directory_uri() . '/dist/js/block-stats.js', ['manifest'], false, true);
+                },
             ]);
         });
     }
