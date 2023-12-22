60e11cb1cbef9c3d3077a13576d1e8d264948813
Ben Everard <ben@theideabureau.co>
2023-12-22 14:36:04 +0000

    Tidied up impact stories JS code

--- resources/js/impact-stories.js
+++ resources/js/impact-stories.js
@@ -4,7 +4,6 @@ import Vue from 'vue'
 const _intersection = require('lodash.intersection');
 
 const reports = new Vue({
-
 	el: '#impact-stories',
 
 	data: {
@@ -15,21 +14,17 @@ const reports = new Vue({
 	},
 
 	watch: {
-
 		filters: {
 			handler: function (val, oldVal) {
 				this.filter();
 			},
 			deep: true
 		}
-
 	},
+
 	methods: {
-
 		filter() {
-
 			Object.keys(this.$refs).forEach(key => {
-
 				const $story = this.$refs[key];
 				const country_ids = JSON.parse($story.getAttribute('data-country-ids'));
 				const story_type_ids = JSON.parse($story.getAttribute('data-story-type-ids'));
@@ -37,26 +32,18 @@ const reports = new Vue({
 				//  remove any existing display styles
 				$story.style.display = null;
 
-				if ( this.filters.type.length ) {
-
-					if ( _intersection(story_type_ids, this.filters.type).length === 0 ) {
+				if (this.filters.type.length) {
+					if (_intersection(story_type_ids, this.filters.type).length === 0) {
 						$story.style.display = 'none';
 					}
-
 				}
 
-				if ( this.filters.country.length ) {
-
-					if ( _intersection(country_ids, this.filters.country).length === 0 ) {
+				if (this.filters.country.length) {
+					if (_intersection(country_ids, this.filters.country).length === 0) {
 						$story.style.display = 'none';
 					}
-
 				}
-
 			});
-
 		}
-
 	},
-
 });
