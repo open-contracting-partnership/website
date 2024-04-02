968c55f9d485f9f990165ad65bc286ffd1e96486
Ben Everard <ben@theideabureau.co>
2024-04-02 10:42:24 +0100

    Replaces yarn with npm, upgrades to Laravel Mix V6

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,48 +13,50 @@ mix.copy('node_modules/mapbox-gl/dist/mapbox-gl.css', 'dist/node_modules/mapbox-
 
 mix.svgSprite('resources/svg', 'svg/icons.svg')
 
-mix.sass('resources/scss/styles.scss', 'dist/css')
-	.sass('resources/scss/gutenberg.scss', 'dist/css')
-	.options({
-		processCssUrls: false,
-        postCss: [tailwindcss('./tailwind.config.js')],
-	});
+mix.options({
+    processCssUrls: false,
+    postCss: [tailwindcss('./tailwind.config.js')],
+});
 
-mix.js('resources/js/scripts.js', 'dist/js')
-	.js('resources/js/element-queries.js', 'dist/js')
-	.js('resources/js/block-download-carousel.js', 'dist/js')
-	.js('resources/js/block-featured-stories-carousel.js', 'dist/js')
-	.js('resources/js/block-our-model.js', 'dist/js')
-	.js('resources/js/block-quote-carousel.js', 'dist/js')
-	.js('resources/js/block-report-header.js', 'dist/js')
-	.js('resources/js/block-stats.js', 'dist/js')
-	.js('resources/js/block-team-profile.js', 'dist/js')
-	.js('resources/js/header.js', 'dist/js')
-	.js('resources/js/impact-stories.js', 'dist/js')
-	.js('resources/js/latest-news.js', 'dist/js')
-	.js('resources/js/archive-resource.js', 'dist/js')
-	.js('resources/js/mailchimp.js', 'dist/js')
-	.js('resources/js/svg.js', 'dist/js')
-	.js('resources/js/modules/worldwide/worldwide.js', 'dist/js')
-	.extract(['vue'])
-	.webpackConfig({
-		resolve: {
-			modules: [
-				'node_modules'
-			]
-		},
-		module: {
-			rules: [
-				{
-					test: /\.scss/,
-					enforce: 'pre',
-					loader: 'import-glob-loader'
-				},
-				{
-					test: /\.js/,
-					enforce: 'pre',
-					loader: 'import-glob-loader'
-				}
-			]
-		}
-	});
+mix.webpackConfig({
+    resolve: {
+        modules: [
+            'node_modules'
+        ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss/,
+                use: 'import-glob-loader'
+            },
+            {
+                test: /\.js/,
+                use: 'import-glob-loader'
+            }
+        ]
+    }
+});
+
+mix.extract(['vue']);
+
+mix
+    .js('resources/js/archive-resource.js', 'dist/js')
+    .js('resources/js/block-download-carousel.js', 'dist/js')
+    .js('resources/js/block-featured-stories-carousel.js', 'dist/js')
+    .js('resources/js/block-our-model.js', 'dist/js')
+    .js('resources/js/block-quote-carousel.js', 'dist/js')
+    .js('resources/js/block-report-header.js', 'dist/js')
+    .js('resources/js/block-stats.js', 'dist/js')
+    .js('resources/js/block-team-profile.js', 'dist/js')
+    .js('resources/js/element-queries.js', 'dist/js')
+    .js('resources/js/header.js', 'dist/js')
+    .js('resources/js/impact-stories.js', 'dist/js')
+    .js('resources/js/latest-news.js', 'dist/js')
+    .js('resources/js/mailchimp.js', 'dist/js')
+    .js('resources/js/modules/worldwide/worldwide.js', 'dist/js')
+    .js('resources/js/scripts.js', 'dist/js')
+    .js('resources/js/svg.js', 'dist/js')
+    .vue()
+    .sass('resources/scss/gutenberg.scss', 'dist/css')
+    .sass('resources/scss/styles.scss', 'dist/css')
