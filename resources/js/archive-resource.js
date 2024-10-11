0a71c2bac6c8a081eb40626fd417193b221683e5
Rachael O'Brien <rachael@theideabureau.co>
2024-10-11 16:59:10 +0100

    adjustments to resources & implement filter logic

--- resources/js/archive-resource.js
+++ resources/js/archive-resource.js
@@ -2,49 +2,36 @@ import _ from 'lodash'
 import Vue from 'vue'
 
 const _cloneDeep = require('lodash.clonedeep');
-const _filter = require('lodash.filter');
-
-// element components
-import Resource from './components/cards/resource.vue'
 
 new Vue({
 
     el: '#resource-archive',
-
-    components: {
-        Resource
-    },
 
     data: {
 
         // data
         resources: content.resources,
 
-        // switch
-        tab: 'resource-library',
-
         // search and filters
         search: '',
-        filter: {}
+        filters: []
 
     },
 
-    watch: {
+    // watch: {
 
-        filter: {
-            deep: true,
-            handler() {
+    //     filter: {
+    //         deep: true,
+    //         handler() {
 
-                let params = this.filter;
+    //             let params = this.filter;
 
-                params.tab = this.tab;
+    //             const url = window.location.pathname + '?' + new URLSearchParams(params).toString();
+    //             history.replaceState(null, '', url);
+    //         }
+    //     }
 
-                const url = window.location.pathname + '?' + new URLSearchParams(params).toString();
-                history.replaceState(null, '', url);
-            }
-        }
-
-    },
+    // },
 
     computed: {
         visibleResources: function () {
@@ -64,26 +51,9 @@ new Vue({
                         }
                     }
 
-                    if (this.tab === 'resource-library') {
-                        if (resource.location.indexOf('resources') === -1) {
-                            display = false;
-                        }
-                    } else {
-                        if (resource.location.indexOf('learning') === -1) {
-                            display = false;
-                        }
-                    }
-
                     // apply resource type filter
-                    if (this.filter['resource-type']) {
-                        if (resource.type !== this.filter['resource-type']) {
-                            display = false;
-                        }
-                    }
-
-                    // apply learning resource category filter
-                    if (this.filter['learning-resource-category']) {
-                        if (resource.learning_resource_category !== this.filter['learning-resource-category']) {
+                    if (this.filters.length) {
+                        if (! this.filters.includes(resource.type)) {
                             display = false;
                         }
                     }
@@ -96,33 +66,9 @@ new Vue({
 
     methods: {
 
-        toggleFilter(event) {
-
-            const value = event.target.value;
-            const taxonomy = event.target.dataset.taxonomy;
-
-            if (this.filter[taxonomy] === undefined) {
-                this.$set(this.filter, taxonomy, value);
-            } else if (this.filter[taxonomy] !== value) {
-                this.$set(this.filter, taxonomy, value);
-            } else {
-                this.$delete(this.filter, taxonomy)
-            }
-
-        },
-
-        isChecked(taxonomy, slug) {
-            return this.filter[taxonomy] === slug;
-        },
-
-        switchTab(event) {
-            this.tab = event.target.dataset.tab;
-            this.resetFilters();
-        },
-
         resetFilters() {
             this.search = '';
-            this.filter = {};
+            this.filters = [];
         }
 
     },
@@ -132,22 +78,9 @@ new Vue({
         const query_string = window.location.search.split('?')[1];
         const params = new URLSearchParams(query_string);
 
-        if (params.has('tab')) {
-
-            if (params.get('tab') === 'resource-library') {
-                this.tab = 'resource-library';
-            } else if (params.get('tab') === 'learning-library') {
-                this.tab = 'learning-library';
-            }
-
-        }
-
-        if (params.has('learning-resource-category')) {
-            this.$set(this.filter, 'learning-resource-category', params.get('learning-resource-category'));
-        }
-
         if (params.has('resource-type')) {
-            this.$set(this.filter, 'resource-type', params.get('resource-type'));
+            this.filters.push(params.get('resource-type'));
+            // this.$set(this.filter, 'resource-type', params.get('resource-type'));
         }
 
     }
