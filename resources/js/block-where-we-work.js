dcbce376efdbbb67c1facfcea52bda70d2d67274
Ben Everard <ben@theideabureau.co>
2024-10-28 13:55:58 +0000

    Adjusted where we work block classes

--- resources/js/block-where-we-work.js
+++ resources/js/block-where-we-work.js
@@ -2,7 +2,7 @@ const whereWeWorkButtons = document.querySelectorAll('.where-we-work__list .butt
 
 if (whereWeWorkButtons) {
     [...whereWeWorkButtons].forEach(button => {
-        const $mapContainer = button.closest('.where-we-work__map-container');
+        const $mapContainer = button.closest('.where-we-work');
 
         button.addEventListener('mouseover', (event) => {
             $mapContainer.dataset.activeRegion = event.target.dataset.region;
