94be5445deb7caf0159dd11c2efd2db6bab11dc2
Ben Everard <ben@theideabureau.co>
2025-11-17 19:57:23 +0000

    Added imgix image signing

--- app/Providers/SEOServiceProvider.php
+++ app/Providers/SEOServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Imgix\UrlBuilder;
 use Rareloop\Lumberjack\Facades\Config;
 use Rareloop\Lumberjack\Providers\ServiceProvider;
 
@@ -20,16 +21,11 @@ class SEOServiceProvider extends ServiceProvider
     public function boot(): void
     {
         // overwrite the yoast og image to use imgix as the domain
-        add_filter('wpseo_opengraph_image', function ($image_url) {
-            $imgix_host_transforms = Config::get('images.imgix_host_transforms');
+        add_filter('wpseo_opengraph_image', function ($imageUrl) {
+            $builder = new UrlBuilder(Config::get('images.imgix_domain'));
+            $builder->setSignKey(Config::get('images.imgix_signing_key'));
 
-            $image_url = str_replace(
-                array_keys($imgix_host_transforms),
-                array_values($imgix_host_transforms),
-                $image_url
-            );
-
-            return $image_url;
+            return $builder->createURL(parse_url($imageUrl)['path'], []);
         });
     }
 }
