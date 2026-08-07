d0c40eefcc6583fee1dde977fc11ff20ac6a8165
AdaBan <ada.banaszak@gmail.com>
2026-08-07 15:50:03 +0200

    Code quality

--- app/Providers/GutenbergServiceProvider.php
+++ app/Providers/GutenbergServiceProvider.php
@@ -25,8 +25,6 @@ class GutenbergServiceProvider extends ServiceProvider
         // blocks
         add_action('init', [$this, 'registerBlockCategories'], 5);
         add_action('init', [$this, 'loadBlocks'], 5);
-        // add_filter('acf/settings/load_json', [$this, 'loadBlockFields']);
-        // add_filter('acf/json/save_paths', [$this, 'saveBlockFields'], 10, 2);
 
         // block fields
         add_action('acf/init', [$this, 'loadBlockFields']);
@@ -77,17 +75,6 @@ class GutenbergServiceProvider extends ServiceProvider
             ]);
         }
     }
-
-    // public static function loadBlockFields(array $paths): array
-    // {
-    //     collect(glob(__DIR__ . '/../../blocks/*'))
-    //         ->map(fn ($blockDirectoryPath) => realpath($blockDirectoryPath))
-    //         ->each(function ($blockDirectoryPath) use (&$paths) {
-    //             $paths[] = $blockDirectoryPath;
-    //         });
-
-    //     return $paths;
-    // }
 
     public function loadBlockFields(): void
     {
