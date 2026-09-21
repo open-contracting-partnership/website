35121f4cca0cffed85e35c067fdb7018e517a3de
AdaBan <ada.banaszak@gmail.com>
2026-09-21 17:38:12 +0200

    Code qualit

--- blocks/card-person/render.php
+++ blocks/card-person/render.php
@@ -6,12 +6,7 @@ $context = Timber::context();
 
 $context['card']['avatar'] = get_field('avatar');
 $context['card']['name'] = get_field('name');
-$context['card']['title'] = get_field('title');
 $context['card']['role'] = get_field('role');
-$context['card']['name_bio'] = implode(', <br/>', array_filter([
-    $context['card']['name'],
-    $context['card']['role'],
-]));
 $context['card']['email_address'] = get_field('email_address');
 $context['card']['twitter_url'] = get_field('twitter_url');
 
