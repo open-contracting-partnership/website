a33bf6535dcc07786a5aead2023f6ec5f1e55873
AdaBan <ada.banaszak@gmail.com>
2026-09-22 17:03:23 +0200

    25242207: card sidebar block

---
+++ blocks/card-sidebar/render.php
@@ -0,0 +1,27 @@
+<?php
+
+use Timber\Timber;
+
+$context = Timber::context();
+
+$context['block'] = [];
+
+$context['block']['title'] = get_field('title');
+$context['block']['description'] = get_field('description');
+$context['block']['image'] = get_field('image');
+$context['block']['colour_scheme'] = get_field('colour_scheme');
+$context['block']['links'] = get_field('links') ?: [];
+
+if ($is_preview) {
+    $context['block']['links'] = array_map(function ($item) {
+        $item['link']['url'] = '#';
+        $item['link']['target'] = '';
+
+        return $item;
+    }, $context['block']['links']);
+}
+
+// options
+$context['block']['options'] = get_field('options') ?: [];
+
+Timber::render('blocks/card-sidebar/card-sidebar.twig', $context);
