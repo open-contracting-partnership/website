e2549a1ce95535e11bb3023ceacb6e4279d85217
AdaBan <ada.banaszak@gmail.com>
2026-09-23 11:13:45 +0200

    fix: cards spacing

--- blocks/card-image/_card-image-fields.php
+++ blocks/card-image/_card-image-fields.php
@@ -1,9 +1,6 @@
 <?php
 
-use Extended\ACF\ConditionalLogic;
 use Extended\ACF\Fields\Image;
-use Extended\ACF\Fields\Link;
-use Extended\ACF\Fields\TrueFalse;
 use Extended\ACF\Location;
 
 register_extended_field_group([
@@ -14,16 +11,6 @@ register_extended_field_group([
             ->library('all')
             ->previewSize('medium')
             ->required(),
-
-        TrueFalse::make('Show Link', 'show_link')
-            ->defaultValue(false),
-
-        Link::make('Link', 'link')
-            ->returnFormat('array')
-            ->required()
-            ->conditionalLogic([
-                ConditionalLogic::where('show_link', '==', 1)
-            ]),
     ],
     'location' => [
         Location::where('block', 'app/card-image'),
