b8ff4ecca0c96c006de47cb58f5b949a4ea1a4d6
Ben Everard <ben@theideabureau.co>
2026-09-24 17:26:27 +0100

    Disabled card border preview link functionality

--- blocks/card-border/render.php
+++ blocks/card-border/render.php
@@ -36,17 +36,6 @@ if (get_field('content') === 'post') {
     $context['block']['buttons'] = get_field('buttons') ?: [];
 }
 
-if ($is_preview) {
-    if ($context['block']['buttons']) {
-        $context['block']['buttons'] = array_map(function ($button) {
-            $button['link']['url'] = '#';
-            $button['link']['target'] = '';
-
-            return $button;
-        }, $context['block']['buttons']);
-    }
-}
-
 // colours
 $context['block']['highlight_colour'] = get_field('highlight_colour') ?: '#000000';
 $context['block']['background_colour'] = get_field('background_colour') ?: '#FFFFFF';
