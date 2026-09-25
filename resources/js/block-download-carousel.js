e765694623156818da1797902d92c4026b092c8c
Ben Everard <ben@theideabureau.co>
2026-09-25 11:20:50 +0100

    Tidied up tiny-slider import

--- resources/js/block-download-carousel.js
+++ resources/js/block-download-carousel.js
@@ -1,4 +1,4 @@
-import { tns } from "../../node_modules/tiny-slider/src/tiny-slider"
+import { tns } from 'tiny-slider';
 
 const $block_download_carousel = document.querySelectorAll('.block[data-block-type="download-carousel"]');
 
